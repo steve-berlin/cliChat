90cc596d0b8a12de460ffd870a3675018d3ed338
steve-berlin <brainasaservice@gmail.com>
2026-01-01 19:09:55 +0100

    Removed bugs found in write and send modules.

--- send.py
+++ send.py
@@ -2,5 +2,5 @@ import subprocess as sp
 
 IP = "192.168.1.50"
 PORT = "8080"
-sp.call(["nc", IP, PORT, "<", "msg.txt"])
+sp.call(f"nc {IP} {PORT} < msg.txt", shell=True)
 print("Message sent!")
--- write.py
+++ write.py
@@ -1,7 +1,12 @@
+# Startup
 txt = 'Write your message here.\n When finished write "END"'
 line = ""
 print(txt)
+# Clear msg.txt
+open("msg.txt", "w").write("")
+# Main texting logic
 while line != "END":
     line = input("\nnext line>> ")
-    with open("msg.txt", "a") as f:
-        f.write(line)
+    if line != "END":
+        with open("msg.txt", "a") as f:
+            f.write(f"{line}\n")
