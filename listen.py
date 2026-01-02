4c8c08c94cea69affc78be725e1cc612dec7131d
steve-berlin <brainasaservice@gmail.com>
2026-01-02 11:06:32 +0100

    Fixed message receiving bug

--- listen.py
+++ listen.py
@@ -2,4 +2,4 @@ import subprocess as sp
 
 PORT = "8080"
 # Redirection works here because shell=True is used
-sp.call(f"nc -l -p {PORT} > msg.txt", shell=True)
+sp.call(f"cd incoming;nc -l -p {PORT} > msg.txt", shell=True)
