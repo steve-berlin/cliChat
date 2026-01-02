9f23c39aa69de1fc19493ad9bc1979c66b60c955
steve-berlin <brainasaservice@gmail.com>
2026-01-02 11:14:33 +0100

    Fixed message receiving bug 2

--- notify.py
+++ notify.py
@@ -5,12 +5,12 @@ from os import system
 
 sfx = audio.from_mp3("sfx.mp3")
 try:
-    with open("incoming.txt", "w") as f:
+    with open("incoming/msg.py.txt", "w") as f:
         f.write("")
 except FileNotFoundError:
-    with open("incoming.txt", "x") as f:
+    with open("incoming/msg.py", "x") as f:
         f.write("")
-incoming = open("incoming.txt", "r").read()
+incoming = open("incoming/msg.py", "r").read()
 while incoming == "":
     sleep(0.5)
 play(sfx)
