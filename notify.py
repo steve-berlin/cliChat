ebb81fea9d3d4eac12c05f57b9349c61c53c8dcc
steve-berlin <brainasaservice@gmail.com>
2026-01-02 11:03:19 +0100

    Improved notify module

--- notify.py
+++ notify.py
@@ -3,7 +3,6 @@ from pydub.playback import play
 from time import sleep
 from os import system
 
-incoming = open("incoming.txt", "r").read()
 sfx = audio.from_mp3("sfx.mp3")
 try:
     with open("incoming.txt", "w") as f:
@@ -11,6 +10,7 @@ try:
 except FileNotFoundError:
     with open("incoming.txt", "x") as f:
         f.write("")
+incoming = open("incoming.txt", "r").read()
 while incoming == "":
     sleep(0.5)
 play(sfx)
